d43d137cdc5e00f934222606e17ceb1adc5a6b41
Sayan Sen <sayansen0361@gmail.com>
2025-09-25 18:23:30 +0530

    Added Update post and delete post functionalities

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -1,5 +1,6 @@
 import { createPostSchema } from "../middlewares/validator.js";
 import { Post } from "../models/postsModel.js";
+import mongoose from "mongoose";
 
 export const getPosts = async (req,res) => {
     const { page } = req.query;
@@ -36,6 +37,11 @@ export const singlePost = async (req,res) => {
                             path : "userId",
                             select : "email",
                         });
+        if(!result) {
+            return res
+                .status(404)
+                .json({ success: false, message: `Post not available` });
+        }
         res.status(200).json({
             success : true,
             message : "Single Post",
@@ -78,9 +84,82 @@ export const createPost = async (req,res) => {
 }
 
 export const updatePost = async (req,res) => {
-    console.log("Hello posts");
+    const { _id } = req.query;
+    const { title, description } = req.body;
+    const { userId } = req.user;
+    try {
+        const {error,value} = createPostSchema.validate({
+                    title,
+                    description,
+                    userId
+                });
+        if (error) {
+            return res
+                .status(401)
+                .json({ success: false, message: error.details[0].message });
+        }
+        const existingPost = await Post.findOne({_id})
+        if(!existingPost) {
+            return res
+                .status(404)
+                .json({ success: false, message: `Post not available` });
+        }
+        if(existingPost.userId.toString() !== userId){
+            return res
+                .status(403)
+                .json({ success: false, message: `Unauthorized to change this post` });
+        }
+        existingPost.title = title;
+        existingPost.description;
+        const result = await existingPost.save();
+        res.status(200).json({
+            success : true,
+            message : "Your post has been updated",
+            data : result
+        })
+    }
+    catch(error){
+        console.log(error);
+    }
 }
 
 export const deletePost = async (req,res) => {
-    console.log("Hello posts");
+    const { _id } = req.query;
+    const { userId } = req.user;
+    try {
+        const existingPost = await Post.findById(_id);
+        if(!existingPost) {
+            return res
+                .status(404)
+                .json({ success: false, message: `No such post available` });
+        }
+        
+        if(existingPost.userId.toString() !== userId){
+            return res
+                .status(403)
+                .json({ success: false, message: `Unauthorized to delete this post` });
+        }
+
+        const deletedPost = await Post.findByIdAndDelete(_id);
+        
+        if (!deletedPost) {
+            return res
+                .status(400)
+                .json({ success: false, message: "Failed to delete post" });
+        }
+
+        return res.status(200).json({
+            success: true,
+            message: "Your post has been deleted",
+            deletedPost 
+        });
+    }
+    catch(error){
+        console.log('Error details:', error);
+        return res.status(500).json({
+            success: false,
+            message: "Error deleting post",
+            error: error.message
+        });
+    }
 }
